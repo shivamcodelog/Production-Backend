45390c4b1e4dfa1b19e01042ab25823c79eba5a4
shivamcodelog <shivamcodelog@gmail.com>
2025-12-24 09:59:56 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))  
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/user",userRouter)
+
+// http://localhost:8000/api/v1/user/register
+
 export {app}
