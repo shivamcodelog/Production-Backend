18dc3a397c999efbbb4d05f49b10765108968af4
shivamcodelog <shivamcodelog@gmail.com>
2025-12-26 21:25:15 +0530

    Add Login functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,26 @@ import {apiError} from "../utils/apiError.js"
 import {User} from "../models/user.models.js"
 import {uploadCloudnary} from "../utils/cloudnary.js"
 import {apiResponse} from "../utils/apiResponse.js"
+
+
+const generateAccessAndRefreshToken=async(userId)=>{
+    try {
+        const user=await User.findById(userId)
+        const accessToken=user.generateAccessToken()
+        const refreshToken=user.generateRefreshToken()
+
+        user.refreshToken=refreshToken
+        await user.save({validateBeforeSave:false})
+
+        return {accessToken,refreshToken}
+         
+        
+    } catch (error) {
+        throw new apiError(500,"Something went wrong while generating referesh and access token")
+        
+    }
+}
+
 const registerUser=asyncHandler(async (req,res)=>{
     // get user details from frontend
     // validation - not empty
@@ -87,7 +107,71 @@ const registerUser=asyncHandler(async (req,res)=>{
    )
 })
 
+// -->Login Functionality
+
+const loginUser= asyncHandler(async(req,res)=>{
+    //req body -->data
+    // username or email
+    // find the user
+    // access and refresh token
+    // send cookie
+
+    const {email,username,password}=req.body
+    if(!username || !email){
+        throw new apiError(400,"username or email is required")
+    }
+    
+    // $or --> find username OR email
+
+    const user=await User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if(!user){
+        throw new apiError(404," User doesn't exist")
+    }
+
+    const isPasswordValid =await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new apiError(401,"Invalid user credentials")
+    }
+
+    const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
 
 
 
-export {registerUser}
+    const loggedInUser=User.findById.
+    select("-password -refreshToken")
+
+    const options={
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new apiResponse(
+            200,
+            {
+                user:loggedInUser,accessToken,
+                refreshToken
+            },
+            "User logged in successfully"
+        )
+    )
+
+})
+
+// --->Logout Functionality
+
+const logoutUser=asyncHandler(async(req,res)=>{
+     
+})
+
+
+
+export {registerUser,loginUser }
