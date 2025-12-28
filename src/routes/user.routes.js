69649464151e1291c8ec14e981eeff198c68bf00
shivamcodelog <shivamcodelog@gmail.com>
2025-12-29 00:26:15 +0530

    Add new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword,
+     getCurrentUser,
+      getUserChannelProfile,
+       getWatchHistory,
+        loginUser,
+         logoutUser,
+          refreshAccessToken,
+           registerUser,
+            updateAcountDetails,
+             updateCoverImage,
+              updateUserAvatar } 
+              from "../controllers/user.controller.js";
+              
 import {upload} from "../middlewares/multer.middleware.js"
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,5 +37,18 @@ router.route("/login").post(loginUser)
 //secured Route
 router.route("/logout").post(verifyJWT , logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/updatedetails").patch(verifyJWT,updateAcountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar")),updateUserAvatar
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage")),updateCoverImage
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
+
 
 export default router
